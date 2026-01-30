d34d0d51b00ffb02b2f66ee2a6cfb7913a82d694
Ayush Garg <gargayush341@gmail.com>
2026-01-30 14:53:19 +0530

    threaded reverse indexing build

--- lab6/revindex_threads.cpp
+++ lab6/revindex_threads.cpp
@@ -3,6 +3,14 @@
 
 using namespace std;
 
+void threadfunc(int idx, vector<wordindex>&results, string filename, string word)
+{
+    wordindex file;
+    file.filename = filename;
+    find_word(&file, word);
+    results[idx] = file;
+}
+
 /*TODO
  * Fill in this function so that it creates a series of threads to execute
  * find_word over different files in parallel.
@@ -22,16 +30,44 @@ using namespace std;
  */
 int run_workers(vector<wordindex>& files, vector<string> filenames,
                 int start_pos, int num_threads, string word) {
-  // for each thread in num_threads:
-  // 1) create a new index object for the file being processes by the thread and
-  //    add it to the files vector
-  // 2) create the thread to run find_word with the proper arguments
+    // for each thread in num_threads:
+    // 1) create a new index object for the file being processes by the thread and
+    //    add it to the files vector
+    // 2) create the thread to run find_word with the proper arguments
 
-  // join with each thread and add the count field of each index to the total
-  // sum
+    // join with each thread and add the count field of each index to the total
+    // sum
 
-  // return the total sum for this batch of files
-  return 0;
+    // return the total sum for this batch of files
+
+    int num_occurrences = 0;
+    int files_size = filenames.size();
+    if (files_size - start_pos >= num_threads) {
+        num_threads = num_threads;
+    }
+    else {
+        num_threads = files_size - start_pos;
+    }
+
+    vector<wordindex>results;
+    results.resize(num_threads);
+    std::vector<std::thread> th;
+    th.reserve(num_threads);
+    for(int i = 0; i < num_threads; i++)
+    {
+        th.emplace_back(threadfunc, i, std::ref(results), filenames[start_pos], word);
+        start_pos += 1;
+    }
+
+    for (auto& t : th) {
+        t.join();
+    }
+
+    for(int i = 0; i != num_threads; ++i) {
+        num_occurrences += results[i].indexes.size();
+        files.push_back(results[i]);
+    }
+    return num_occurrences;
 }
 
 /* The main REPL for the program
@@ -43,71 +79,71 @@ int run_workers(vector<wordindex>& files, vector<string> filenames,
  * Return value: None
  */
 void repl(char* dirname) {
-  int num_files = 0;
-  int workers = 8;
-  vector<wordindex> files;
-  vector<string> filenames;
+    int num_files = 0;
+    int workers = 8;
+    vector<wordindex> files;
+    vector<string> filenames;
 
-  // read all files from directory
-  get_files(filenames, dirname);
-  num_files = filenames.size();
+    // read all files from directory
+    get_files(filenames, dirname);
+    num_files = filenames.size();
 
-  files.reserve(num_files);
+    files.reserve(num_files);
 
-  printf("Enter search term: ");
-  char buf[255];
-  fgets(buf, 255, stdin);
-  while (!feof(stdin)) {
-    int len = char_traits<char>::length(buf) - 1;
-    string term(buf, len);
-    if (term.length() > 0) {
-      int num_occurrences = 0;
-      transform(term.begin(), term.end(), term.begin(), ::tolower);
+    printf("Enter search term: ");
+    char buf[255];
+    fgets(buf, 255, stdin);
+    while (!feof(stdin)) {
+        int len = char_traits<char>::length(buf) - 1;
+        string term(buf, len);
+        if (term.length() > 0) {
+            int num_occurrences = 0;
+            transform(term.begin(), term.end(), term.begin(), ::tolower);
 
-      int completed = 0;
-      while (completed < num_files) {
-        // determine number of threads for this round
-        int num_threads = 0;
-        if ((num_files - completed) > workers) {
-          num_threads = workers;
-        } else {
-          num_threads = num_files - completed;
+            int completed = 0;
+            while (completed < num_files) {
+                // determine number of threads for this round
+                int num_threads = 0;
+                if ((num_files - completed) > workers) {
+                    num_threads = workers;
+                } else {
+                    num_threads = num_files - completed;
+                }
+
+                // increment the total number of occurrences of the search term by the
+                // sum of the occurrences from this batch of files files is passed by
+                // reference so that additions to the vector in run_workers is visable
+                // back in this function
+                num_occurrences +=
+                        run_workers(files, filenames, completed, num_threads, term);
+
+                // update the number of completed files
+                completed += num_threads;
+
+                // move on to next batch of "workers" amount of files
+            }
+
+            // print the results
+            printOccurrences(term, num_occurrences, files);
         }
 
-        // increment the total number of occurrences of the search term by the
-        // sum of the occurrences from this batch of files files is passed by
-        // reference so that additions to the vector in run_workers is visable
-        // back in this function
-        num_occurrences +=
-            run_workers(files, filenames, completed, num_threads, term);
+        printf("\nEnter search term: ");
+        files.clear();
 
-        // update the number of completed files
-        completed += num_threads;
-
-        // move on to next batch of "workers" amount of files
-      }
-
-      // print the results
-      printOccurrences(term, num_occurrences, files);
+        fgets(buf, 255, stdin);
     }
-
-    printf("\nEnter search term: ");
-    files.clear();
-
-    fgets(buf, 255, stdin);
-  }
-  return;
+    return;
 }
 
 int main(int argc, char* argv[]) {
-  if (argc < 2) {
-    fprintf(stderr, "Expected arguments: files to search through\n");
-    exit(1);
-  }
+    if (argc < 2) {
+        fprintf(stderr, "Expected arguments: files to search through\n");
+        exit(1);
+    }
 
-  char* dirname = argv[1];
+    char* dirname = argv[1];
 
-  repl(dirname);
+    repl(dirname);
 
-  exit(0);
+    exit(0);
 }
